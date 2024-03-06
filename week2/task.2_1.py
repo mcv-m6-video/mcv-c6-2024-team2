01385630964d1a6173ac176bba51080204b6815d
Miruna-Diana Jarda <miruna@atlas-merge.com>
2024-03-06 13:25:18 +0100

    Add week 2 - task 1.1, 2.1

---
+++ week2/task.2_1.py
@@ -0,0 +1,150 @@
+import cv2
+import numpy as np
+from utils import parse_xml_annotations
+from PIL import Image, ImageDraw
+import os
+import json
+
+dir_path = 'AICity_data/train/S03/c010'
+video_path = dir_path + '/vdo.avi'
+roi_path = dir_path + '/roi.jpg'
+xml_path = dir_path + '/ai_challenge_s03_c010-full_annotation.xml'
+frames_path = 'frames_roi/'
+json_file_path = 'predictions_all.json'
+
+img = cv2.imread(roi_path, cv2.IMREAD_GRAYSCALE)
+cap = cv2.VideoCapture(video_path)
+
+width = cap.get(3) 
+height = cap.get(4) 
+
+gt_boxes = parse_xml_annotations(xml_file=xml_path)
+with open(json_file_path, 'r') as file:
+    json_data = json.load(file)
+
+def calculate_iou(box1, box2):
+    x1 = max(box1[0], box2[0])
+    y1 = max(box1[1], box2[1])
+    x2 = min(box1[2], box2[2])
+    y2 = min(box1[3], box2[3])
+
+    width_intersection = max(0, x2 - x1)
+    height_intersection = max(0, y2 - y1)
+
+    area_intersection = width_intersection * height_intersection
+
+    area_box1 = (box1[2] - box1[0]) * (box1[3] - box1[1])
+    area_box2 = (box2[2] - box2[0]) * (box2[3] - box2[1])
+
+    area_union = area_box1 + area_box2 - area_intersection
+
+    iou = area_intersection / area_union if area_union > 0 else 0.0
+    return iou
+
+def images_to_gif(frames, output_path):
+    gif_frames = [cv2.cvtColor(frame, cv2.COLOR_BGR2RGB) for frame in frames]
+    gif_frames = [Image.fromarray(frame) for frame in gif_frames]
+    gif_frames[0].save(output_path, 
+            save_all = True, append_images = gif_frames[1:], 
+            optimize = True, duration = 10, loop=0, quality=80) 
+
+def track_video(cap, start_frame=0, end_frame=None, mode='show', roi=False):
+    current_frames = {}
+    previous_frames = {}
+    tracking_id = 1
+
+    threshold = 0.2
+
+    gif_frames = []
+
+    font = cv2.FONT_HERSHEY_SIMPLEX 
+    fontScale = 1
+    color = (255, 0, 0) 
+    thickness = 2
+
+    frame_id = start_frame
+    cap.set(cv2.CAP_PROP_POS_FRAMES, start_frame)
+
+    # Play the video
+    while cap.isOpened(): 
+
+        # Capture frame-by-frame 
+        ret, frame = cap.read() 
+
+        if not ret or (end_frame is not None and frame_id >= end_frame):
+            break
+
+        previous_frames = current_frames
+        current_frames = {}
+
+        if roi:
+            roi_coord = np.where(img == 0)
+            frame[roi_coord[0], roi_coord[1]] = 0
+            
+        curr_frame = 'frame' + str(frame_id) + '.png'
+        if frame_id == 0:
+            # for b in json_data[curr_frame]:
+            for b in gt_boxes[frame_id]:
+                x = int(b['xtl'])
+                y = int(b['ytl'])
+                x_2 = int(b['xbr'])
+                y_2 = int(b['ybr'])
+
+                b = [x, y, x_2, y_2]
+
+                # b = list(map(int, b))
+                current_frames[tracking_id] = b
+                tracking_id += 1
+
+        else:
+            # if curr_frame in json_data:
+            if frame_id in gt_boxes:
+                # for b in json_data[curr_frame]:
+                for b in gt_boxes[frame_id]:
+                    x = int(b['xtl'])
+                    y = int(b['ytl'])
+                    x_2 = int(b['xbr'])
+                    y_2 = int(b['ybr'])
+
+                    b = [x, y, x_2, y_2]
+
+                    highest_overlap = {}
+                    # b = list(map(int, b))
+
+                    for k, prev_b in previous_frames.items():
+                        iou = calculate_iou(b, prev_b)
+                        if iou > threshold:
+                            highest_overlap[k] = iou
+                        
+                    if len(highest_overlap.keys()) > 0:
+                        key_max = max(highest_overlap, key=highest_overlap.get)
+                        current_frames[key_max] = b
+
+                    else:
+                        current_frames[tracking_id] = b
+                        tracking_id += 1
+
+
+        for k, b in current_frames.items():
+            x, y, x_2, y_2 = b
+            cv2.putText(frame, str(k), (x, y), font, fontScale, color, thickness)
+            cv2.rectangle(frame, (x, y), (x_2, y_2), (0, 255, 0), 2)
+
+
+        if mode == 'show':
+            cv2.imshow('Foreground detection', frame)
+        elif mode == 'write':
+            cv2.imwrite(f'{frames_path}frame{frame_id}.png', frame)
+
+        gif_frames.append(frame)
+
+        frame_id += 1
+
+        # Press Q on keyboard to exit 
+        if cv2.waitKey(30) & 0xFF == ord('q'):
+            break
+
+    images_to_gif(gif_frames, 'tracked_gt_2.gif')
+
+
+track_video(cap, start_frame=320, end_frame=365)
---
+++ week2/task1_1.py
@@ -0,0 +1,96 @@
+# Setup detectron2 logger
+from detectron2.utils.logger import setup_logger
+setup_logger()
+
+# import some common libraries
+import numpy as np
+import os, json, cv2, random
+import time
+import shutil
+from tqdm import tqdm
+
+from utils import parse_xml_annotations
+
+# import some common detectron2 utilities
+from detectron2.evaluation import COCOEvaluator, inference_on_dataset
+from detectron2.data import build_detection_test_loader
+from detectron2.structures import BoxMode
+from detectron2 import model_zoo
+from detectron2.engine import DefaultPredictor
+from detectron2.config import get_cfg
+from detectron2.utils.visualizer import Visualizer
+from detectron2.data import MetadataCatalog, DatasetCatalog
+
+
+path_output = './outputs/'
+path_input = './frames/'
+path_xml = './ai_challenge_s03_c010-full_annotation.xml'
+model = 'COCO-Detection/faster_rcnn_R_50_FPN_3x'
+threshold = 0.5
+
+
+cfg = get_cfg()
+cfg.merge_from_file(model_zoo.get_config_file(f"{model}.yaml"))
+cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = threshold  # set threshold for this model
+cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url(f"{model}.yaml")
+predictor = DefaultPredictor(cfg)
+
+def get_images(img_dir):
+    images = [img for img in os.listdir(img_dir)]
+
+    return sorted(images, key=lambda x: x[0])
+
+def get_frame_dicts():
+    img_dir = path_input
+    xml_path = path_xml
+    height = 1080
+    width = 1920
+
+    gt_boxes = parse_xml_annotations(xml_file=xml_path)
+
+    images = get_images(img_dir)
+    print(images)
+
+    dataset_dicts = []
+    for i, img in enumerate(images):
+
+        img_item = {}
+        img_item['file_name'] = img_dir + img
+        img_item['height']= height
+        img_item['width']= width
+        img_item['image_id']= i
+
+        objs = []
+        if i in gt_boxes:
+            for b in gt_boxes[i]:
+                x = int(b['xtl'])
+                y = int(b['ytl'])
+                x_2 = int(b['xbr'])
+                y_2 = int(b['ybr'])
+
+                bbox = [x, y, x_2, y_2]
+                bbox = list(map(float, bbox))
+
+                if b['label'] == 'bycicle':
+                    class_id = 1
+                elif b['label'] == 'car':
+                    class_id = 0    
+
+                obj = {
+                    "bbox": bbox,
+                    "bbox_mode": BoxMode.XYXY_ABS,
+                    "category_id": class_id,
+                }
+                objs.append(obj)
+        
+        img_item['annotations'] = objs
+        dataset_dicts.append(img_item)
+    
+    return dataset_dicts
+
+DatasetCatalog.register("seq_03", get_frame_dicts)
+MetadataCatalog.get("seq_03").set(thing_classes=MetadataCatalog.get(cfg.DATASETS.TRAIN[0]).thing_classes)
+
+evaluator = COCOEvaluator("seq_03", output_dir="evaluate_results")
+val_loader = build_detection_test_loader(cfg, "seq_03")
+print(inference_on_dataset(predictor.model, val_loader, evaluator))
